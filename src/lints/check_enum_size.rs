947601a3ccdcd8ae4e532e42bc04d759cdac150b
wuaoxiang <wuaoxiang@stargraph.cn>
2021-10-17 15:37:27 +0800

    WIP: check_enum_size lint

---
+++ src/lints/check_enum_size.rs
@@ -0,0 +1,65 @@
+pub(super) struct CheckEnumSize;
+
+impl CheckEnumSize {
+    const NAME: &'static str = std::any::type_name::<Self>();
+    const LINT: rustc_lint::Lint = {
+        let mut lint = rustc_lint::Lint::default_fields_for_macro();
+        lint.name = Self::NAME; // can't use LintPass::name(), because trait cannot declared const fn
+        lint.default_level = rustc_lint::Level::Warn;
+        lint
+    };
+}
+
+impl rustc_lint::LintPass for CheckEnumSize {
+    fn name(&self) -> &'static str {
+        Self::NAME
+    }
+}
+
+impl<'tcx> rustc_lint::LateLintPass<'tcx> for CheckEnumSize {
+    fn check_item(&mut self, cx: &rustc_lint::LateContext<'_>, item: &rustc_hir::Item<'_>) {
+ 
+        if let rustc_hir::ItemKind::Enum(ref _def, _) = item.kind {
+            let ty = cx.tcx.type_of(item.def_id);
+            let adt = ty.ty_adt_def().expect("already checked whether this is an enum");
+            dbg!(&adt.variants);
+            let mut temp = vec![];
+            for variant in &adt.variants {
+                // dbg!(variant.ident);
+                let mut variant_size = 0;
+                for filed in &variant.fields {
+                    let ty = cx.tcx.type_of(filed.did);
+                    // dbg!(ty);
+                    let type_ = rustc_middle::ty::layout::LayoutOf::layout_of(cx, ty).unwrap();
+                    // dbg!(type_.size.bytes());
+                    variant_size += type_.size.bytes();
+                }
+                temp.push((variant_size, variant.ident.to_string()));
+            }
+            // dbg!(temp);
+            let mut max_size_index = 0;
+            let mut max_size = 0;
+            let mut min_size_index = 0;
+            let mut min_size = u64::MAX;
+            for (idx, (size, _ident)) in temp.iter().enumerate() {
+                let size = *size;
+                if size < min_size {
+                    min_size_index = idx;
+                    min_size = size;
+                }
+                if size > max_size {
+                    max_size_index = idx;
+                    max_size = size;
+                }
+            }
+            dbg!(min_size_index, max_size_index);
+            if temp[max_size_index].0 - temp[min_size_index].0 > 200 {
+                rustc_lint::LintContext::struct_span_lint(cx, &Self::LINT, item.span, |diagnostic| {
+                    let mut diagnostic = diagnostic.build("enum size has waste");
+                    diagnostic.emit();
+                });
+            }
+        
+        }
+    }
+}
--- src/lints/mod.rs
+++ src/lints/mod.rs
@@ -1,5 +1,10 @@
 mod fn_name_is_foo;
+mod check_enum_size;
 
 pub(super) fn register_early_pass_lints(lint_store: &mut rustc_lint::LintStore) {
     lint_store.register_early_pass(|| Box::new(fn_name_is_foo::FnNameIsFoo));
 }
+
+// pub(super) fn register_late_pass_lints(lint_store: &mut rustc_lint::LintStore) {
+//     lint_store.register_late_pass(|| Box::new(check_enum_size::CheckEnumSize));
+// }
